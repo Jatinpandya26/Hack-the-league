2377821216f1277cbfba89bf3a0d9ad962bcf0fc
Manjeet Dular M-Jay <2001mr.king@gmail.com>
2022-02-01 16:31:39 +0530

    workshop

--- js/main.js
+++ js/main.js
@@ -145,13 +145,4 @@
 }(jQuery));
 /* Sessions and workshop 
 =========================================================*/
-$(document).ready(function ($) {
-
-  $('.card__share > a').on('click', function (e) {
-    e.preventDefault() // prevent default action - hash doesn't appear in url
-    $(this).parent().find('div').toggleClass('card__social--active');
-    $(this).toggleClass('share-expanded');
-  });
-
-});
 
